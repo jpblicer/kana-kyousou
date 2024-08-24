63eb8a5ded451d8623447e7fa86a429771aa23f0
James Young <jpblicer@gmail.com>
2024-08-24 15:27:23 +0900

    Create Hiragana Class methods generate a random kana generate 3 uniq kana
    inside of pages controller
    make sure the random kana is not in the 3 wrong kana otherwise generate again
    pass both the random and 3 incorrect kana back to the hiragana model
    
    Hiragana class method that combines the possible answers together and shuffles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @random_hiragana = Hiragana.random_kana
+    wrong_answers = Hiragana.wrong_answers
+    wrong_answers = Hiragana.wrong_answers while wrong_answers.include?(@random_hiragana)
+    @possible_answers = Hiragana.possible_answers(@random_hiragana, wrong_answers)
   end
 end
--- app/models/hiragana.rb
+++ app/models/hiragana.rb
@@ -1,2 +1,14 @@
 class Hiragana < ApplicationRecord
+  def self.random_kana
+    Hiragana.all.sample
+  end
+
+  def self.wrong_answers
+    Hiragana.all.sample(3).uniq
+  end
+
+  def self.possible_answers(correct_answer, wrong_answers)
+    possible_answers = wrong_answers << correct_answer
+    possible_answers.shuffle
+  end
 end
