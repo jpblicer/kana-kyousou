103f1159db016b9c446e4c903cf1b07ef3f97ebe
James Young <jpblicer@gmail.com>
2024-09-03 18:57:13 +0900

    set up controller and event works

--- app/javascript/controllers/flashcards_controller.js
+++ app/javascript/controllers/flashcards_controller.js
@@ -2,7 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="flashcards"
 export default class extends Controller {
+  static targets = [ "flashcard" ]
+
   connect() {
     console.log("Flashcard Controller connected")
+  }
+
+  checkAnswer() {
+    console.log("Click Test")
   }
 }
